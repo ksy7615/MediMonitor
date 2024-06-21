eff78ba6b7255c8656f0d420750b90813da12bf1
ksy7615 <sykim7615@daum.net>
2024-06-21 11:10:20 +0900

    Create Project

---
+++ src/main/java/com/example/medimonitor/MedimonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.medimonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedimonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedimonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/medimonitor/MedimonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medimonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedimonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
