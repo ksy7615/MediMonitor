aad5e1986c17dd9069e56103e6e35b443530fbf4
yoon-wish <sowon5685@gmail.com>
2024-06-24 12:43:05 +0900

    Add main.jsp and Update SearchController (use ModelAndView)

--- src/main/java/com/example/medimonitor/pacs/study/controller/SearchController.java
+++ src/main/java/com/example/medimonitor/pacs/study/controller/SearchController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -17,11 +18,15 @@ public class SearchController {
 
     private final StudyService studyService;
 
-    @ResponseBody
     @GetMapping("/main")
-    public ResponseEntity<List<Study>> getAllStudies(){
+    public ModelAndView getAllStudies() {
         List<Study> result = studyService.findAll();
+        ModelAndView mv = new ModelAndView("main/main"); // "main"은 main 폴더의 main.jsp 파일을 의미합니다.
+        mv.addObject("studies", result); // "studies"라는 이름으로 결과 리스트를 추가합니다.
 
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        return mv;
     }
+
+
+
 }
