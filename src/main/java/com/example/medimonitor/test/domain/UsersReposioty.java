4ef8b733f7144c48506cb74973cd8fc028b1ef51
ksy7615 <sykim7615@daum.net>
2024-06-24 18:39:46 +0900

    Delete : test package

--- src/main/java/com/example/medimonitor/test/domain/UsersReposioty.java
+++
@@ -1,6 +0,0 @@
-package com.example.medimonitor.test.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersReposioty extends JpaRepository<Users, String> {
-}
