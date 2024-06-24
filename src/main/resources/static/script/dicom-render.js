d83c2f345f4adeffda5386c2737ff29db7a3ebb3
iconew123 <iconew123@gmail.com>
2024-06-24 16:04:43 +0900

    Tools init

--- src/main/resources/static/script/dicom-render.js
+++ src/main/resources/static/script/dicom-render.js
@@ -1,37 +1,45 @@
-import * as cornerstone from '@cornerstonejs/core'
-import * as cornerstoneDICOMImageLoader from '@cornerstonejs/dicom-image-loader'
-import * as dicomParser from 'dicom-parser'
-
-const input = document.getElementById("file");
+import * as cornerstone from '@cornerstonejs/core';
+import * as cornerstoneDICOMImageLoader from '@cornerstonejs/dicom-image-loader';
+import * as dicomParser from 'dicom-parser';
+import { init as csToolsInit } from "@cornerstonejs/tools";
+import * as cornerstoneTools from "@cornerstonejs/tools";
 
 // 뷰 포트 생성
 const content = document.getElementById('content');
 const element = document.createElement('div');
+
+// 툴 정의
+const {
+    MagnifyTool,
+    TrackballRotateTool,
+    ZoomTool,
+    ToolGroupManager,
+    Enums: csToolsEnums,
+} = cornerstoneTools;
+
+const { MouseBindings } = csToolsEnums;
+
+element.oncontextmenu = (e) => e.preventDefault();
 element.style.width = '500px';
 element.style.height = '500px';
-
 content.appendChild(element);
 
 // 파일 리딩
-input.addEventListener("change", e => {
-    // 파일이 변화하면
-    // 파일을 읽고 -> 버퍼 (바이너리 데이터를 가져와)
-    // ImageId를 생성 (dicomweb://)
-    // 이미지 렌더링을 위한 render 메소드 완성
+const input = document.getElementById("file");
 
+input.addEventListener("change", (e) => {
     const files = e.target.files;
 
     const reader = new FileReader();
     reader.onload = (file) => {
         const data = file.target.result;
         render(data);
-    }
+    };
     reader.readAsArrayBuffer(files[0]);
-})
+});
 
-const render = (arrayBuffer) => {
-    // Get Cornerstone imageIds and fetch metadata into RAM
-    const imageId = `dicomweb:${URL.createObjectURL(new Blob([arrayBuffer], {type : 'application/dicom'}))}`;
+const render = async (arrayBuffer) => {
+    const imageId = `dicomweb:${URL.createObjectURL(new Blob([arrayBuffer], { type: 'application/dicom' }))}`;
     console.log('imageId : ', imageId);
 
     const imageIds = [imageId];
@@ -47,12 +55,47 @@ const render = (arrayBuffer) => {
     };
 
     renderingEngine.enableElement(viewportInput);
-
     const viewport = renderingEngine.getViewport(viewportInput.viewportId);
 
-    viewport.setStack(imageIds, 0);
+    await viewport.setStack(imageIds, 0);
 
+    // 이미지가 로드된 후에 툴 설정을 수행
+    setTools(viewportId, renderingEngineId);
+
+    // 뷰포트 리랜더링
     viewport.render();
+};
+
+const setTools = (viewportId, renderingEngineId) => {
+    // 툴 추가
+    csToolsInit();
+
+    const toolGroupId = 'NAVIGATION_TOOL_GROUP_ID';
+
+    cornerstoneTools.addTool(MagnifyTool);
+    cornerstoneTools.addTool(TrackballRotateTool);
+    cornerstoneTools.addTool(ZoomTool);
+
+    const toolGroup = ToolGroupManager.createToolGroup(toolGroupId);
+
+    toolGroup.addTool(MagnifyTool.toolName, { cursor: 'move' });
+    toolGroup.addTool(TrackballRotateTool.toolName, { cursor: 'crosshair' });
+    toolGroup.addTool(ZoomTool.toolName, { cursor: 'zoom-in' });
+
+    // 툴 활성화
+    toolGroup.setToolActive(MagnifyTool.toolName, {
+        bindings: [{ mouseButton: MouseBindings.Primary }],
+    });
+
+    toolGroup.setToolActive(TrackballRotateTool.toolName, {
+        bindings: [{ mouseButton: MouseBindings.Auxiliary }],
+    });
+
+    toolGroup.setToolActive(ZoomTool.toolName, {
+        bindings: [{ mouseButton: MouseBindings.Secondary }],
+    });
+
+    toolGroup.addViewport(viewportId, renderingEngineId);
 }
 
 const init = async () => {
@@ -73,7 +116,8 @@ const init = async () => {
             },
         },
     };
+
     cornerstoneDICOMImageLoader.webWorkerManager.initialize(config);
-}
+};
 
 init();
--- src/main/resources/static/webpack.config.js
+++ src/main/resources/static/webpack.config.js
@@ -1,14 +1,41 @@
 const path = require("path");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
 
 module.exports = {
     entry: './script/dicom-render.js',      // 시작 파일의 경로 명세
     output: {
         filename: 'bundle.js',              // 출력 파일의 이름
-        path: path.resolve(__dirname,'dist')// 출력 디렉토리
+        path: path.resolve(__dirname, 'dist')// 출력 디렉토리
     },
     resolve: {
-        modules:['node_modules'],             // 모듈 검색 경로
-        extensions: ['.ts','.js', 'json', '.wasm']
+        modules: ['node_modules'],             // 모듈 검색 경로
+        extensions: ['.ts', '.js', 'json', '.wasm'],
+        fallback: {
+            "fs": false,
+            "path": require.resolve("path-browserify"),
+            "os": require.resolve("os-browserify/browser"),
+            "module": false
+        }
     },
-    mode: 'development'                     // 개발 모드
-}
+    cache: {
+        type: 'filesystem',
+        cacheDirectory: path.resolve(__dirname, '.webpack_cache'),
+    },
+    mode: 'development',                     // 개발 모드
+    module: {
+        rules: [
+            {
+                test: /\.wasm$/,
+                type: 'webassembly/async'
+            }
+        ]
+    },
+    experiments: {
+        asyncWebAssembly: true
+    },
+    plugins: [
+        new NodePolyfillPlugin()
+    ]
+};
+
