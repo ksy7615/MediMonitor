a30f63419215bf7804a9457cc594e583085ac018
yoon-wish <sowon5685@gmail.com>
2024-06-25 17:12:15 +0900

    Add FindByPid

--- src/main/java/com/example/medimonitor/pacs/study/controller/SearchController.java
+++ src/main/java/com/example/medimonitor/pacs/study/controller/SearchController.java
@@ -5,10 +5,9 @@ import com.example.medimonitor.pacs.study.service.StudyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -17,10 +16,16 @@ public class SearchController {
     private final StudyService studyService;
 
     @CrossOrigin
-    @GetMapping("/mainData")
+    @GetMapping("/mainAllSearch")
     @ResponseBody
     public Page<Study> getAllStudies(@RequestParam(defaultValue="0") int page, @RequestParam(defaultValue = "5") int size ) {
         return studyService.findStudiesWithPagination(page, size);
+    }
+
+    @GetMapping("/mainPrevious/{pId}")
+    @ResponseBody
+    public List<Study> getStudiesByPid(@PathVariable String pId) {
+        return studyService.getStudiesByPid(pId);
     }
 
     @GetMapping("/main")
