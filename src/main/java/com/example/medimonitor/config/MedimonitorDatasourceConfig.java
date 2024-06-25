bb2b5a20d1ef997ab5fc73a7139df8d5e280fc9c
ksy7615 <sykim7615@daum.net>
2024-06-25 11:47:43 +0900

    Modify : Medimonitor config 오타 수정

--- src/main/java/com/example/medimonitor/config/MedimonitorDatasourceConfig.java
+++ src/main/java/com/example/medimonitor/config/MedimonitorDatasourceConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -45,8 +46,9 @@ class MediMonitorDatasourceConfig {
                 .build();
     }
 
+    @Primary
     @Bean(name = "medimonitorEntityManager")
-    public LocalContainerEntityManagerFactoryBean entityaManagerFactory() {
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
 
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         vendorAdapter.setGenerateDdl(true);
@@ -64,6 +66,7 @@ class MediMonitorDatasourceConfig {
         return factory;
     }
 
+    @Primary
     @Bean(name = "medimonitorTransactionManager")
     public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
 
