8915e5d375eb182e655266837da75769a39fcca8
c-chae-eun-n <ce.hue125@gmail.com>
2024-06-25 13:56:56 +0900

    Update : UserRepository Repository 어노테이션 추가

--- src/main/java/com/example/medimonitor/user/domain/UserRepository.java
+++ src/main/java/com/example/medimonitor/user/domain/UserRepository.java
@@ -1,6 +1,8 @@
 package com.example.medimonitor.user.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
 }
