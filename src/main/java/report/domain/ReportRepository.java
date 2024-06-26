13f4e3dfa0c7e34040083736be3ca15dbdcb62e3
yoon-wish <sowon5685@gmail.com>
2024-06-26 17:54:52 +0900

    Modify DB 두 개 연결 연습

--- src/main/java/report/domain/ReportRepository.java
+++ src/main/java/report/domain/ReportRepository.java
@@ -1,6 +1,9 @@
 package report.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReportRepository extends JpaRepository<Report, String> {
+
 }
