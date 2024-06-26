6deaac99e7b1e0be1ea8577ac975fa682f001e3a
c-chae-eun-n <ce.hue125@gmail.com>
2024-06-26 14:02:04 +0900

    Add : UserRepository findByAuthorityFalse 추가

--- src/main/java/com/example/medimonitor/user/domain/UserRepository.java
+++ src/main/java/com/example/medimonitor/user/domain/UserRepository.java
@@ -3,6 +3,10 @@ package com.example.medimonitor.user.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+
+    public List<User> findByAuthorityFalse();
 }
