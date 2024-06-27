a7df2bffef6d6262d0b27e036208217a7533fae9
c-chae-eun-n <ce.hue125@gmail.com>
2024-06-27 18:23:31 +0900

    Modify : 임시 header.js

---
+++ src/main/resources/static/script/header.js
@@ -0,0 +1,19 @@
+document.getElementById('message-icon').addEventListener('click', function() {
+    document.getElementById('message-dropdown').classList.toggle('show');
+});
+
+document.getElementById('user-icon').addEventListener('click', function() {
+    document.getElementById('user-dropdown').classList.toggle('show');
+});
+
+window.onclick = function(event) {
+    if (!event.target.matches('.icon')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
