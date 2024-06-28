5895942cb8623c904b925d4258fe9a1df5663194
ksy7615 <sykim7615@daum.net>
2024-06-28 10:06:33 +0900

    Add: 검색 메소드 추가 in StudyService

--- src/main/java/com/example/medimonitor/pacs/study/service/StudyService.java
+++ src/main/java/com/example/medimonitor/pacs/study/service/StudyService.java
@@ -2,6 +2,7 @@ package com.example.medimonitor.pacs.study.service;
 
 import com.example.medimonitor.pacs.study.domain.Study;
 import com.example.medimonitor.pacs.study.domain.StudyRepository;
+import com.example.medimonitor.pacs.study.dto.StudyRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -22,9 +24,25 @@ public class StudyService {
         return studyRepository.findAll(pageable);
     }
 
-
-
     public List<Study> getStudiesByPid(String pId){
         return studyRepository.findByPid(pId);
+    }
+
+    public List<Study> findByPidLike(String pid){
+        return studyRepository.findByPidLike("%" + pid + "%");
+    }
+
+    public List<Study> findByReportstatus(long reportStatus){
+        return studyRepository.findByReportstatus(reportStatus);
+    }
+
+
+    public List<Study> findByModality(String modality){
+        return studyRepository.findByModality(modality);
+    }
+
+
+    public List<Study> findByPnameLike(String pname){
+        return studyRepository.findByPnameLike("%" + pname + "%");
     }
 }
