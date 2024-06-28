e836939d94d57c2ee1253753f2973619adc0e017
ksy7615 <sykim7615@daum.net>
2024-06-28 10:07:01 +0900

    Add: 검색 관련 Repository 추가

--- src/main/java/com/example/medimonitor/pacs/study/domain/StudyRepository.java
+++ src/main/java/com/example/medimonitor/pacs/study/domain/StudyRepository.java
@@ -3,9 +3,19 @@ package com.example.medimonitor.pacs.study.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface StudyRepository extends JpaRepository<Study, Long> {
     List<Study> findByPid(String Pid);
+
+    List<Study> findByPidLike(String Pid);
+
+    List<Study> findByReportstatus(long reportStatus);
+
+    List<Study> findByModality(String modality);
+
+    List<Study> findByPnameLike(String Pname);
+
 }
