9bec58f85a5c861f7300f19faf5d0ac6884ea65e
iconew123 <iconew123@gmail.com>
2024-06-28 17:43:40 +0900

    Modify View Controller

--- src/main/java/com/example/medimonitor/pacs/image/controller/ImageController.java
+++ src/main/java/com/example/medimonitor/pacs/image/controller/ImageController.java
@@ -1,33 +1,24 @@
 package com.example.medimonitor.pacs.image.controller;
 
-import com.example.medimonitor.pacs.image.domain.Image;
 import com.example.medimonitor.pacs.image.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:8080") // 필요한 도메인으로 변경
 public class ImageController {
 
     private final ImageService imageService;
 
+    // View Controller >> DicomInfo Page
     @GetMapping("/detail/{studyKey}/{seriesKey}")
     public String showImagesPage(@PathVariable(name = "studyKey") long studyKey, @PathVariable(name = "seriesKey") long seriesKey, Model model) {
         model.addAttribute("studyKey", studyKey);
         model.addAttribute("seriesKey", seriesKey);
-        return "dicomInfo"; // 뷰 템플릿 이름
+        return "dicomInfo";
     }
 
-    @GetMapping("/api/images/{studyKey}/{seriesKey}")
-    @ResponseBody
-    public List<Image> getImages(@PathVariable(name = "studyKey") long studyKey, @PathVariable(name = "seriesKey") long seriesKey) {
-        System.out.println("study key: " + studyKey);
-        System.out.println("series key: " + seriesKey);
-        return imageService.findAllImage(studyKey, seriesKey);
-    }
 }
