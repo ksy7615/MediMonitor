0a76bf563db90368bddfbf06a410217ac5bc2ffa
c-chae-eun-n <ce.hue125@gmail.com>
2024-06-28 18:19:14 +0900

    Update : menuButton.js 사용자 삭제 함수 추가

--- src/main/resources/static/script/menuButton.js
+++ src/main/resources/static/script/menuButton.js
@@ -9,6 +9,37 @@ function toggleDropdownMenu() {
     dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
 }
 
+function deleteBtnMenu() {
+    let selectedUsers = [];
+    $(".userCheckbox:checked").each(function() {
+        selectedUsers.push($(this).data('username'));
+    });
+
+    if (selectedUsers.length > 0) {
+        var settings = {
+            "url": "/user/reject",
+            "method": "DELETE",
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "data": JSON.stringify(selectedUsers),
+        };
+
+        $.ajax(settings).done(function (response) {
+            if(response.status === 200) {
+                alert(response.message);
+                location.reload();
+            }
+        }).fail(function (response) {
+            if(response.status === 400) {
+                alert(response.message);
+            }
+        });
+    } else {
+        alert('삭제할 사용자를 선택해 주세요.');
+    }
+}
+
 // 선택된 사용자를 승인하는 기능
 document.getElementById('approveBtn').addEventListener('click', function() {
     let selectedUsers = [];
@@ -37,7 +68,7 @@ document.getElementById('approveBtn').addEventListener('click', function() {
             }
         });
     } else {
-        alert('승인할 사용자를 선택해주세요.');
+        alert('승인할 사용자를 선택해 주세요.');
     }
 });
 
@@ -69,6 +100,6 @@ document.getElementById('rejectBtn').addEventListener('click', function() {
             }
         });
     } else {
-        alert('거절할 사용자를 선택해주세요.');
+        alert('거절할 사용자를 선택해 주세요.');
     }
 });
