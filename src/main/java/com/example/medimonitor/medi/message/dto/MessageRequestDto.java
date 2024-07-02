ee2189f44969a2e597293fa67211010201ed7655
c-chae-eun-n <ce.hue125@gmail.com>
2024-07-02 11:48:05 +0900

    Create : MessageRequestDto.java

---
+++ src/main/java/com/example/medimonitor/medi/message/dto/MessageRequestDto.java
@@ -0,0 +1,28 @@
+package com.example.medimonitor.medi.message.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class MessageRequestDto {
+
+    private int code;
+    private String title;
+    private String content;
+    private String recipient;
+    private String sender;
+    private boolean status;
+    private Timestamp regDate;
+    private Timestamp modDate;
+
+    public String getFormattedRegDate() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        return sdf.format(this.regDate);
+    }
+}
