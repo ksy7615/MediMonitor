1c2fbd5760cbf2aebc353d7b0328e0f14b0a2618
c-chae-eun-n <ce.hue125@gmail.com>
2024-07-04 12:45:52 +0900

    Update : MessageService.java save 메소드 추가

--- src/main/java/com/example/medimonitor/medi/message/service/MessageService.java
+++ src/main/java/com/example/medimonitor/medi/message/service/MessageService.java
@@ -1,6 +1,9 @@
 package com.example.medimonitor.medi.message.service;
 
+import com.example.medimonitor.medi.message.domain.Message;
 import com.example.medimonitor.medi.message.domain.MessageRepository;
+import com.example.medimonitor.medi.message.dto.MessageRequestDto;
+import com.example.medimonitor.medi.message.dto.MessageResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +11,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class MessageService {
     private final MessageRepository messageRepository;
+
+    public MessageResponseDto save(MessageRequestDto messageDto) {
+        Message message = new Message(messageDto);
+        MessageResponseDto messageResponseDto = new MessageResponseDto(messageRepository.save(message));
+        return messageResponseDto;
+    }
 }
