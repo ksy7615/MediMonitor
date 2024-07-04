c1928f2e1c7853981c3bff013fb333003abf7a4b
iconew123 <iconew123@gmail.com>
2024-07-04 10:20:01 +0900

    Create Response(json) DICOM Series Info And Imagefiles Encoding.

--- src/main/java/com/example/medimonitor/pacs/image/controller/ImageRestController.java
+++ src/main/java/com/example/medimonitor/pacs/image/controller/ImageRestController.java
@@ -2,6 +2,8 @@ package com.example.medimonitor.pacs.image.controller;
 
 import com.example.medimonitor.pacs.image.domain.Image;
 import com.example.medimonitor.pacs.image.service.ImageService;
+import com.example.medimonitor.pacs.series.domain.Series;
+import com.example.medimonitor.pacs.series.service.SeriesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,9 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.List;
+import java.util.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/image")
@@ -21,35 +21,42 @@ import java.util.List;
 public class ImageRestController {
 
     private final ImageService imageService;
+    private final SeriesService seriesService;
 
     // RestAPI Controller
 
     @PostMapping("/{studykey}/{serieskey}")
-    public List<String> getAllImageDatas(@PathVariable long studykey, @PathVariable long serieskey)  {
-        List<String> list = new ArrayList<String>();
+    public Map<String, Object> getAllImageDatas(@PathVariable long studykey, @PathVariable long serieskey)  {
+        List<String> fileList = new ArrayList<>();
 
         List<Image> images = imageService.findAllImage(studykey, serieskey);
+        List<Series> series = seriesService.findAllSeriesByStudyKey(studykey, serieskey);
 
         for (Image image : images) {
             // 실제 파일경로 추출
-            String filePath =  getRealFilePath(image.getPath(), image.getFname());
+            String filePath = getRealFilePath(image.getPath(), image.getFname());
 
             File file = new File(filePath);
-            if(file.exists()) {
-                try {
-                    FileInputStream fis = new FileInputStream(file);
+            if (file.exists()) {
+                try (FileInputStream fis = new FileInputStream(file)) {
                     byte[] bytes = fis.readAllBytes();
 
                     // JSON 응답 타입의 경우, byte[]에 대한 유실의 위험이 있음
                     // 안정적인 Base64 타입으로 인코딩하여 전달
                     String imageData = Base64.getEncoder().encodeToString(bytes);
-                    list.add(imageData);
+                    fileList.add(imageData);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
-        return list;
+
+        // series의 정보와
+        Map<String, Object> response = new HashMap<>();
+        response.put("fileList", fileList);
+        response.put("seriesInfo", series);
+
+        return response;
     }
 
     private String getRealFilePath(String path, String filename) {
@@ -60,12 +67,13 @@ public class ImageRestController {
 
         String rootPath = "Z:\\";
 
-        if(osName.equals("mac"))
+        if (osName.equals("mac")) {
             rootPath = "\\Volumes\\STS\\";
+        }
 
         realPath += rootPath;
         realPath += path + filename;
-        realPath.replace("\\", separator);
+        realPath = realPath.replace("\\", separator);
 
         return realPath;
     }
