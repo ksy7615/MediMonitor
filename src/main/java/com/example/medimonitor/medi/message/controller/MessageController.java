85d8f6be79661c41b0ba9ba8f200e8317c444200
c-chae-eun-n <ce.hue125@gmail.com>
2024-07-04 12:45:15 +0900

    Update : MessageController.java write 메소드 추가

--- src/main/java/com/example/medimonitor/medi/message/controller/MessageController.java
+++ src/main/java/com/example/medimonitor/medi/message/controller/MessageController.java
@@ -1,9 +1,17 @@
 package com.example.medimonitor.medi.message.controller;
 
+import com.example.medimonitor.medi.message.dto.MessageRequestDto;
+import com.example.medimonitor.medi.message.dto.MessageResponseDto;
 import com.example.medimonitor.medi.message.service.MessageService;
+import com.example.medimonitor.util.Response;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
 @Controller
@@ -16,5 +24,24 @@ public class MessageController {
 
     @GetMapping("/write")
     public String write() {return "message/write";}
+
+    @ResponseBody
+    @PostMapping("/write")
+    public ResponseEntity<Response> write(@RequestBody MessageRequestDto messageDto) {
+        Response response = new Response();
+
+        MessageResponseDto message = messageService.save(messageDto);
+        if(message != null) {
+            response.setMessage("쪽지 전송 완료");
+            response.setStatus(200);
+
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }else {
+            response.setMessage("쪽지 전송 실패");
+            response.setStatus(400);
+
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+    }
 
 }
