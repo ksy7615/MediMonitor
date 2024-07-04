c4adbe600e7347956c280fb032c576c4ec4d61c6
ksy7615 <sykim7615@daum.net>
2024-07-04 15:41:10 +0900

    Delete: report status를 Table Report에서 참조하기 때문에 삭제

--- src/main/java/com/example/medimonitor/pacs/study/domain/StudyRepository.java
+++ src/main/java/com/example/medimonitor/pacs/study/domain/StudyRepository.java
@@ -13,7 +13,6 @@ import java.util.List;
 public interface StudyRepository extends JpaRepository<Study, Long> {
     List<Study> findByPid(String pId);
     Page<Study> findByPidLike(String pId, Pageable pageable);
-    Page<Study> findByReportstatus(long reportStatus, Pageable pageable);
     Page<Study> findByModality(String modality, Pageable pageable);
     Page<Study> findByPnameLike(String pname, Pageable pageable);
     Page<Study> findByStudydateBetween(String startDate, String endDate, Pageable pageable);
@@ -22,12 +21,10 @@ public interface StudyRepository extends JpaRepository<Study, Long> {
     @Query("SELECT s FROM Study s WHERE " +
             "(:pid IS NULL OR s.pid LIKE %:pid%) AND " +
             "(:pname IS NULL OR s.pname LIKE %:pname%) AND " +
-            "(:reportstatus IS NULL OR s.reportstatus = :reportstatus) AND " +
             "(:modality IS NULL OR s.modality LIKE %:modality%) AND " +
             "(:startDate IS NULL OR :endDate IS NULL OR s.studydate BETWEEN :startDate AND :endDate)")
     Page<Study> search(@Param("pid") String pid,
                        @Param("pname") String pname,
-                       @Param("reportstatus") String reportstatus,
                        @Param("modality") String modality,
                        @Param("startDate") String startDate,
                        @Param("endDate") String endDate,
