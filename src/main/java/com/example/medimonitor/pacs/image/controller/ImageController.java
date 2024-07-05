7832b1f840cb6ba585730a06b384972c6a8009e7
iconew123 <iconew123@gmail.com>
2024-07-05 15:19:58 +0900

    Move controller method

--- src/main/java/com/example/medimonitor/pacs/image/controller/ImageController.java
+++ src/main/java/com/example/medimonitor/pacs/image/controller/ImageController.java
@@ -17,11 +17,11 @@ public class ImageController {
     private final ImageService imageService;
 
     // View Controller >> DicomInfo Page
-    @GetMapping("/detail/{studyKey}/{seriesKey}")
-    public String showImagesPage(@PathVariable(name = "studyKey") long studyKey, @PathVariable(name = "seriesKey") long seriesKey, Model model) {
+    @GetMapping("/detail/{studyKey}/{studyDate}")
+    public String getDetailView(@PathVariable(name = "studyKey") long studyKey,@PathVariable(name = "studyDate") String studyDate, Model model) {
         model.addAttribute("studyKey", studyKey);
-        model.addAttribute("seriesKey", seriesKey);
-        return "dicomInfo";
+        model.addAttribute("studyDate", studyDate);
+        return "detail";
     }
 
     // 이거 지우지 마세요 저 써야 돼요 지우지 마세요 제발요 제발 지우지 마세요
