8a6734cd7ed82fa52800a9f01581d19b8bbebea5
ksy7615 <sykim7615@daum.net>
2024-07-05 11:24:45 +0900

    Modify: 스터디 접근 시 log save 위치 변경 + path 수정 완료

--- src/main/java/com/example/medimonitor/pacs/series/controller/SeriesController.java
+++ src/main/java/com/example/medimonitor/pacs/series/controller/SeriesController.java
@@ -1,69 +1,18 @@
 package com.example.medimonitor.pacs.series.controller;
 
-import com.example.medimonitor.medi.log.domain.Log;
-import com.example.medimonitor.medi.log.domain.LogService;
-import com.example.medimonitor.medi.log.dto.LogRequestDto;
-import com.example.medimonitor.medi.user.dto.UserResponseDto;
 import com.example.medimonitor.pacs.series.domain.Series;
-import com.example.medimonitor.pacs.series.domain.SeriesRepository;
 import com.example.medimonitor.pacs.series.service.SeriesService;
-import com.example.medimonitor.pacs.study.domain.Study;
-import com.example.medimonitor.pacs.study.domain.StudyRepository;
-import com.example.medimonitor.pacs.study.service.StudyService;
-import com.example.medimonitor.util.Response;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @Controller
 public class SeriesController {
 
-    private final LogService logService;
-    private final SeriesRepository seriesRepository;
     private final SeriesService seriesService;
-
-    @PostMapping("/detail/{studykey}")
-    @ResponseBody
-    public ResponseEntity<Response> saveLog(@PathVariable("studykey") long studykey, HttpSession session) {
-        UserResponseDto userResponseDto = (UserResponseDto) session.getAttribute("user");
-        String username = userResponseDto != null ? userResponseDto.getUsername() : null;
-
-        System.out.println("username : " + username);
-        System.out.println("study key : " + studykey);
-
-        LogRequestDto logRequestDto = new LogRequestDto();
-        logRequestDto.setStudykey(studykey);
-        logRequestDto.setUsername(username);
-
-        Log log = null;
-        Response response = new Response();
-
-        String message = "조회에 성공하였습니다.";
-
-        log = logService.save(logRequestDto);
-
-        System.out.println("log : " + log);
-
-        if (log != null) {
-            response.setStatus(200);
-            response.setMessage(message);
-
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        } else {
-            response.setStatus(400);
-            response.setMessage("조회 실패");
-
-            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-        }
-    }
 
     @GetMapping("/thumbnail/{studykey}")
     @ResponseBody
--- src/main/java/com/example/medimonitor/pacs/series/controller/SeriesRestController.java
+++ src/main/java/com/example/medimonitor/pacs/series/controller/SeriesRestController.java
@@ -1,12 +1,21 @@
 package com.example.medimonitor.pacs.series.controller;
 
+import com.example.medimonitor.medi.log.domain.Log;
+import com.example.medimonitor.medi.log.domain.LogService;
+import com.example.medimonitor.medi.log.dto.LogRequestDto;
+import com.example.medimonitor.medi.user.dto.UserResponseDto;
 import com.example.medimonitor.pacs.series.domain.Series;
+import com.example.medimonitor.pacs.series.domain.SeriesRepository;
 import com.example.medimonitor.pacs.series.service.SeriesService;
 import com.example.medimonitor.pacs.study.domain.Study;
 import com.example.medimonitor.pacs.study.service.StudyService;
+import com.example.medimonitor.util.Response;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,14 +25,18 @@ import java.util.Map;
 @RestController
 public class SeriesRestController {
 
+    private final LogService logService;
     private final SeriesService seriesService;
     private final StudyService studyService;
 
     @GetMapping("/detail/{studykey}")
     @ResponseBody
-    public Map<String, Object> findAllSeriesByStudykey(@PathVariable("studykey") long studykey) {
+    public Map<String, Object> findAllSeriesByStudykey(@PathVariable("studykey") long studykey, HttpSession session) {
         Study study = studyService.findByStudykey(studykey).stream().findFirst().orElse(null);
         List<String> seriesKeyList = seriesService.findAllSerieskey(studykey);
+
+        // 로그 저장
+        saveLog(studykey, session);
 
         // 정보 뽑아오기 ( Study 의 정보 + 시리즈키 값만 )
         Map<String, Object> response = new HashMap<>();
@@ -31,5 +44,17 @@ public class SeriesRestController {
         response.put("seriesList", seriesKeyList);
 
         return response;
+    }
+
+    // 로그 저장을 위한 메서드
+    private void saveLog(long studykey, HttpSession session) {
+        UserResponseDto userResponseDto = (UserResponseDto) session.getAttribute("user");
+        String username = userResponseDto != null ? userResponseDto.getUsername() : null;
+
+        LogRequestDto logRequestDto = new LogRequestDto();
+        logRequestDto.setStudykey(studykey);
+        logRequestDto.setUsername(username);
+
+        logService.save(logRequestDto);
     }
 }
