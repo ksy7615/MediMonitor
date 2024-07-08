194b60b620d45f54e31c0f14e3841926434d7c87
c-chae-eun-n <ce.hue125@gmail.com>
2024-07-08 18:05:40 +0900

    Update : MessageService.java updateStatus 메소드 추가

--- src/main/java/com/example/medimonitor/medi/message/domain/MessageRepository.java
+++ src/main/java/com/example/medimonitor/medi/message/domain/MessageRepository.java
@@ -1,8 +1,10 @@
 package com.example.medimonitor.medi.message.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,4 +21,8 @@ public interface MessageRepository extends JpaRepository<Message, Integer> {
     @Query(nativeQuery = true, value = "SELECT * FROM message WHERE sender=?1 ORDER BY reg_date DESC LIMIT 30 OFFSET 0")
     public List<Message> findBySenderLimit(String sender);
 
+    @Modifying
+    @Transactional
+    @Query(nativeQuery = true, value = "UPDATE message SET status=1 WHERE code=?1")
+    public int updateStatus(int code);
 }
