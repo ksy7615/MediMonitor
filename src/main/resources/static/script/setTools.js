7099ccadd3b88fe9978c781def42daf29d5a4aff
iconew123 <iconew123@gmail.com>
2024-07-09 21:34:00 +0900

    Add Tools

--- src/main/resources/static/script/setTools.js
+++ src/main/resources/static/script/setTools.js
@@ -1,48 +1,80 @@
-import {init as csToolsInit, MagnifyTool, TrackballRotateTool, ZoomTool , StackScrollMouseWheelTool} from "@cornerstonejs/tools";
+import {
+    init as csToolsInit,
+    ZoomTool,
+    WindowLevelTool,
+    PanTool,
+    StackScrollMouseWheelTool,
+    removeTool, LengthTool, ProbeTool
+} from "@cornerstonejs/tools";
 import * as cornerstoneTools from "@cornerstonejs/tools";
+const { ToolGroupManager } = cornerstoneTools;
+const { MouseBindings } = cornerstoneTools.Enums;
 
-const {ToolGroupManager, Enums: csToolsEnums} = cornerstoneTools;
-const {MouseBindings} = csToolsEnums;
+let toolGroup;
 
-const toolGroups = new Map();
-
-const setTools = (viewportId, renderingEngineId) => {
+const setTools = (viewports, renderingEngineId, toolGroupId) => {
     // 툴 초기화
     csToolsInit();
 
-    // 툴 그룹 uid생성
-    const toolGroupId = `toolGroupId-${viewportId}`;
-    console.log('toolId : ' + toolGroupId);
-
-    // 툴을 항상 추가 시도하고, 이미 추가된 경우 예외를 무시
-    try {
-        cornerstoneTools.addTool(StackScrollMouseWheelTool);
-    } catch (e) {
-        if (e.message !== "StackScrollMouseWheel has already been added globally") {
-            console.log(e);
-        }
-    }
-
-    // 툴 그룹이 이미 존재하는지 확인하고 가져오기
-    let toolGroup = toolGroups.get(toolGroupId);
     if (!toolGroup) {
+        // 툴 그룹 ID 생성 및 툴 그룹 생성 (툴 그룹은 한 번만 생성)
+        toolGroupId = `toolGroupId`;
+        console.log('toolId : ' + toolGroupId);
+
+        // 툴 그룹에 툴 추가
+        cornerstoneTools.addTool(StackScrollMouseWheelTool);
+        cornerstoneTools.addTool(ZoomTool);
+        cornerstoneTools.addTool(WindowLevelTool);
+        cornerstoneTools.addTool(PanTool);
+        cornerstoneTools.addTool(LengthTool);
+
         // 툴 그룹 생성
         toolGroup = ToolGroupManager.createToolGroup(toolGroupId);
+        console.log(toolGroup);
 
-        // 툴 추가
+        // 툴 적용
         toolGroup.addTool(StackScrollMouseWheelTool.toolName);
+        toolGroup.addTool(ZoomTool.toolName);
+        toolGroup.addTool(WindowLevelTool.toolName);
+        toolGroup.addTool(PanTool.toolName);
+        toolGroup.addTool(LengthTool.toolName);
 
-        // 툴 활성화
-        toolGroup.setToolActive(StackScrollMouseWheelTool.toolName);
-
-        // 툴 그룹을 Map에 저장
-        toolGroups.set(toolGroupId, toolGroup);
+        // 툴 활성화 (기본 도구 설정)
+        toolGroup.setToolActive(StackScrollMouseWheelTool.toolName, {
+            bindings: [{ mouseButton: MouseBindings.Auxiliary }],
+        });
     }
 
-    // 뷰포트에 툴 적용
-    toolGroup.addViewport(viewportId, renderingEngineId);
-
-    console.log(toolGroup);
+    // 모든 뷰포트에 툴 그룹 적용
+    viewports.forEach(viewport => {
+        const viewportId = `CT_AXIAL_STACK-${viewport.id}`;
+        toolGroup.addViewport(viewportId, renderingEngineId);
+    });
 }
 
-export { setTools };
+const activateTool = (toolName, toolGroupId) => {
+    const toolGroup = ToolGroupManager.getToolGroup(toolGroupId);
+    console.log(toolGroup);
+    console.log(toolName.toolName);
+    if (toolGroup) {
+        const toolNames = [
+            ZoomTool.toolName,
+            WindowLevelTool.toolName,
+            PanTool.toolName,
+            LengthTool.toolName
+        ];
+
+        toolNames.forEach(tool => {
+            toolGroup.setToolPassive(tool, {
+                removeAllBindings : true
+            });
+        });
+
+        toolGroup.setToolActive(toolName.toolName, {
+            bindings: [{ mouseButton: MouseBindings.Primary }],
+        });
+
+    }
+}
+
+export { setTools, activateTool };
