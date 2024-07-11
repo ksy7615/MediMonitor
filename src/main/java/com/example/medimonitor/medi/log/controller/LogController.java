03419451ad2d198a5dcf85998bba24609d67bd14
ksy7615 <sykim7615@daum.net>
2024-07-11 11:59:30 +0900

    Add: Log Controller & 접근 기록 리스트 추가

---
+++ src/main/java/com/example/medimonitor/medi/log/controller/LogController.java
@@ -0,0 +1,25 @@
+package com.example.medimonitor.medi.log.controller;
+
+import com.example.medimonitor.medi.log.domain.Log;
+import com.example.medimonitor.medi.log.dto.LogRequestDto;
+import com.example.medimonitor.medi.log.service.LogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class LogController {
+
+    private final LogService logService;
+
+    @PostMapping("/log")
+    @ResponseBody
+    public List<Log> findLogList (@RequestBody LogRequestDto logRequestDto) {
+        return logService.findAllLogByUsername(logRequestDto.getUsername());
+    }
+}
