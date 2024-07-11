485b9cc2d267a7490e346b57faaf835781bbe334
ksy7615 <sykim7615@daum.net>
2024-07-11 12:00:17 +0900

    Add: username으로 Log List 찾는 쿼리 추가

--- src/main/java/com/example/medimonitor/medi/log/domain/LogRepository.java
+++ src/main/java/com/example/medimonitor/medi/log/domain/LogRepository.java
@@ -2,6 +2,8 @@ package com.example.medimonitor.medi.log.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LogRepository extends JpaRepository<Log, Integer> {
+import java.util.List;
 
+public interface LogRepository extends JpaRepository<Log, Integer> {
+    List<Log> findLogByUsername (String username);
 }
