dc86b7888139f5864ddba7e513f6c1e9d268e6e4
ksy7615 <sykim7615@daum.net>
2024-07-11 12:00:44 +0900

    Add: Log List 서비스 추가

--- src/main/java/com/example/medimonitor/medi/log/service/LogService.java
+++ src/main/java/com/example/medimonitor/medi/log/service/LogService.java
@@ -6,15 +6,24 @@ import com.example.medimonitor.medi.log.dto.LogRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class LogService {
 
     private final LogRepository logRepository;
 
-    public Log save(LogRequestDto logRequestDto) {
+    public Log save (LogRequestDto logRequestDto) {
         Log log = new Log(logRequestDto);
         return logRepository.save(log);
     }
 
+    public List<Log> findAllLogByUsername (String username) {
+        if(username.equals("admin")) {
+            return logRepository.findAll();
+        } else {
+            return logRepository.findLogByUsername(username);
+        }
+    }
 }
