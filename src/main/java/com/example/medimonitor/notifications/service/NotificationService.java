61e586a4c1d69a73e51a264d7d59a76728bac837
c-chae-eun-n <ce.hue125@gmail.com>
2024-07-14 17:21:06 +0900

    Update : NotificationService.java

--- src/main/java/com/example/medimonitor/notifications/service/NotificationService.java
+++ src/main/java/com/example/medimonitor/notifications/service/NotificationService.java
@@ -1,34 +1,92 @@
 package com.example.medimonitor.notifications.service;
 
+import com.example.medimonitor.medi.user.domain.User;
+import com.example.medimonitor.medi.user.domain.UserRepository;
+import com.example.medimonitor.medi.user.dto.UserResponseDto;
 import com.example.medimonitor.notifications.domain.NotificationRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import com.example.medimonitor.notifications.event.NotificationEvent;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
 
 @Service
+@RequiredArgsConstructor
 public class NotificationService {
 
+
     private final NotificationRepository notificationRepository;
-    private final ApplicationEventPublisher eventPublisher;
+    private final UserRepository userRepository;
 
-    @Autowired
-    public NotificationService(NotificationRepository notificationRepository, ApplicationEventPublisher eventPublisher) {
-        this.notificationRepository = notificationRepository;
-        this.eventPublisher = eventPublisher;
+    private static final Long DEFAULT_TIMEOUT = 600L * 1000 * 60;
+
+    public SseEmitter sendNotification(String username) {
+        SseEmitter emitter = createEmitter(username);
+
+        sendToClient(username, "EventStream Created. [username=" + username + "]", "sse 접속 성공");
+        return emitter;
     }
 
-    public void notifyUser(String userId, String message) {
-        notificationRepository.addNotification(userId);
-        int newCount = notificationRepository.getNotificationCount(userId);
-        eventPublisher.publishEvent(new NotificationEvent(this, userId, message, newCount));
+    public <T> void customNotify(String username, T data, String comment, String type) {
+        sendToClient(username, data, comment, type);
     }
 
-    public int getNotificationCount(String userId) {
-        return notificationRepository.getNotificationCount(userId);
+    public void notify(String username, Object data, String comment) {
+        sendToClient(username, data, comment);
     }
 
-    public void resetNotificationCount(String userId) {
-        notificationRepository.resetNotificationCount(userId);
+    private void sendToClient(String username, Object data, String comment) {
+        SseEmitter emitter = notificationRepository.get(username);
+        if (emitter != null) {
+            try {
+                emitter.send(SseEmitter.event()
+                        .id(String.valueOf(username))
+                        .name("sse")
+                        .data(data)
+                        .comment(comment));
+            } catch (IOException e) {
+                notificationRepository.deleteById(username);
+                emitter.completeWithError(e);
+            }
+        }
     }
+
+
+    private <T> void sendToClient(String username, T data, String comment, String type) {
+        SseEmitter emitter = notificationRepository.get(username);
+        if (emitter != null) {
+            try {
+                emitter.send(SseEmitter.event()
+                        .id(String.valueOf(username))
+                        .name(type)
+                        .data(data)
+                        .comment(comment));
+                System.out.println("SSE message sent to " + username + ": " + data); // 로그 추가
+            } catch (IOException e) {
+                notificationRepository.deleteById(username);
+                emitter.completeWithError(e);
+                System.err.println("Failed to send event to " + username + ": " + e.getMessage());
+            }
+        } else {
+            System.err.println("No SSE emitter found for " + username); // 로그 추가
+        }
+    }
+
+    private SseEmitter createEmitter(String username) {
+        SseEmitter emitter = new SseEmitter(DEFAULT_TIMEOUT);
+        notificationRepository.save(username, emitter);
+
+        emitter.onCompletion(() -> notificationRepository.deleteById(username));
+        emitter.onTimeout(() -> notificationRepository.deleteById(username));
+
+        return emitter;
+    }
+
+    private UserResponseDto validUser(String username) {
+        String userId = String.valueOf(username);
+        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+        UserResponseDto userDto = new UserResponseDto(user);
+        return userDto;
+    }
+
 }
